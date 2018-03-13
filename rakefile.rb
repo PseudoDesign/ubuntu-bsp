f7a85cd058c6ae62df749b4d8a62981b6898a06f
Adam <PseudoDesign@users.noreply.github.com>
2018-03-13 00:48:07 -0600

    installing modules

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,8 @@ require "/vagrant/borgrake.rb"
 
 uboot_dir = '/home/vagrant/uboot-fslc'
 kernel_dir = '/home/vagrant/linux-fslc'
+ubuntu_rfs_dir = '/home/vagrant/ubuntu-core'
+install_dir = '/home/vagrant/install'
 
 def crossmake(target)
   arch = "arm"
@@ -30,9 +32,10 @@ task :kernel do
 end
 
 task :install => [:kernel, :uboot] do
-  install_dir = "/share/install"
   binary_dir = File.join(install_dir, "binary")
+  rfs_dir = File.join(install_dir, "media/rootfs")
   FileUtils.mkdir_p binary_dir
+  FileUtils.mkdir_p rfs_dir
   # TODO: Verify .bin is the correct file to load
   FileUtils.cp(
     File.join(uboot_dir, "u-boot.bin"),
@@ -43,7 +46,13 @@ task :install => [:kernel, :uboot] do
     binary_dir
   )
   FileUtils.cp(
-    File.join(kernel_dir, "arch", "arm", "boot", "dts", "imx6dqscm-1gb-qwks-rev3-btwifi-fix-ldo.dtb"),
+    File.join(
+      kernel_dir, "arch", "arm", "boot", "dts",
+      "imx6dqscm-1gb-qwks-rev3-btwifi-fix-ldo.dtb"
+    ),
     binary_dir
   )
+  sh "sudo cp -r #{ubuntu_rfs_dir}/* #{rfs_dir}"
+  sh "sudo make -C #{kernel_dir} modules_install firmware_install \
+      INSTALL_MOD_PATH=#{rfs_dir} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- "
 end
