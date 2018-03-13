53eb70c9addaab238b7554d3dc4d7bf356546baa
Adam <PseudoDesign@users.noreply.github.com>
2018-03-12 23:17:44 -0600

    added build uboot ot rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,2 +1,13 @@
 
 require "/vagrant/borgrake.rb"
+
+task :make_uboot do
+  uboot_dir = '/home/vagrant/uboot-fslc'
+  uboot_config = 'mx6sabresd_defconfig'
+  arch = "arm"
+  cross_compile = "arm-linux-gnueabihf-"
+  Dir.chdir(uboot_dir) do
+    sh "make ARCH=#{arch} CROSS_COMPILE=#{cross_compile} #{uboot_config}"
+    sh "make ARCH=#{arch} CROSS_COMPILE=#{cross_compile} -j4"
+  end
+end
